4d8b3d9735340b72015601413ac515e3eb2343f4
Quentin Tennerel <tennerelquentin@gmail.com>
2026-03-29 12:38:17 +0800

    feat: add sessions (#32)

--- liquibase/scripts/V1__triggers.sql
+++ liquibase/scripts/V1__triggers.sql
@@ -1,7 +1,3 @@
 CREATE TRIGGER trg_users_updated_at
 BEFORE UPDATE ON users
 FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER trg_resources_updated_at
-BEFORE UPDATE ON resources
-FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
