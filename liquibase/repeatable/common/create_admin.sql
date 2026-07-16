a3d9463879cbb6ecf79d980f4a1f86241e552ee8
Quentin Tennerel <tennerelquentin@gmail.com>
2026-07-16 02:20:47 +0200

    fix: fix admin first connect

--- liquibase/repeatable/common/create_admin.sql
+++ liquibase/repeatable/common/create_admin.sql
@@ -1,3 +1,7 @@
 INSERT INTO users (first_name, last_name, email, password, status, is_archived)
 VALUES ('Admin', 'User', 'template.email@gmail.com', 'password_template', 'active', FALSE)
 ON CONFLICT (email) DO NOTHING;
+
+UPDATE users
+SET first_connect = FALSE
+WHERE id = 1;
